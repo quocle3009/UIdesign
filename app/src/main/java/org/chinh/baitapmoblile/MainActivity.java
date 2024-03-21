68cb339ff7bacf0879b7df76a06a2b1967cccc56
quocle3009 <quocle300903@gmail.com>
2024-03-21 14:00:49 +0700

    done

---
+++ app/src/main/java/org/chinh/baitapmoblile/MainActivity.java
@@ -0,0 +1,18 @@
+package org.chinh.baitapmoblile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.Bundle;
+
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
